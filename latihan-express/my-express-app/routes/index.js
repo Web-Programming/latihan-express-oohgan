8829592920f805ad2cb70e04a1bde7d61b80dcbc
Steven Chandra <stevenchandrafei@gmail.com>
2024-10-02 09:31:08 +0700

    add: new project latihan-express/my-express-app

---
+++ latihan-express/my-express-app/routes/index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "My World" });
+});
+
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "About Us" });
+});
+
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "Contact Us" });
+});
+module.exports = router;
